7f848d9c87f2f89d016ef7c59cb1e118b14e55a8
martins0023 <martinsmiracle45@gmail.com>
2025-11-29 15:00:08 +0100

    upgrade

--- constants/index.jsx
+++ constants/index.jsx
@@ -1,4 +1,4 @@
-import { Edit3, HelpCircle, Home, Info, Phone } from "lucide-react";
+import { Edit3, HelpCircle, Home, Info, Phone, User2Icon } from "lucide-react";
 
 export const navItems = [
   { label: "Home", href: "https://doctorkays.com", iconMapping: <Home className="w-5 h-5 inline mr-2" /> },
@@ -8,6 +8,51 @@ export const navItems = [
   // { label: "Shop", href: "/shop", iconMapping: <ShoppingCart className="w-5 h-5 inline mr-2" /> },
   { label: "Contact", href: "https://doctorkays.com/contact", iconMapping: <Phone className="w-5 h-5 inline mr-2" /> },
   { label: "FAQs", href: "https://doctorkays.com/faqs", iconMapping: <HelpCircle className="w-5 h-5 inline mr-2" /> },
+];
+
+export const testimonials = [
+  {
+    user: "MLS Abimbola",
+    country: "United Kingdom",
+    image: User2Icon,
+    text: "Service is very satisfying.",
+    rating: 5,
+  },
+  {
+    user: "Philadolar",
+    country: "Nigeria",
+    image: User2Icon,
+    text: "Your product or content has been really educative and also fun to watch. Keep it up",
+    rating: 5,
+  },
+  {
+    user: "Oluitan Olumide",
+    country: "Nigeria",
+    image: User2Icon,
+    text: "Doctor kays has been an amazing Doctor since I got to know him.",
+    rating: 5,
+  },
+  {
+    user: "Jeremiah Robert",
+    country: "Nigeria",
+    image: User2Icon,
+    text: "Dr Kays is now a household name in social media space. The people on my space are loving it, to the extent of asking: this Doctor kay's you are always posting is doing very good with the health education. Those tips and reminders are very helpul.",
+    rating: 5,
+  },
+  {
+    user: "Commy-Constance Oko",
+    country: "Nigeria",
+    image: User2Icon,
+    text: "I have learnt so much about personal health and got to know a lot on health matters generally. Thank you so much Dr. Kays for always giving prompt response to my questions.",
+    rating: 5,
+  },
+  {
+    user: "Ezekiel",
+    country: "London",
+    image: User2Icon,
+    text: "Great team and the app is easy to use.",
+    rating: 5,
+  },
 ];
 
 export const resourcesLinks = [
